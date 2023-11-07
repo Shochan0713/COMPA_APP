a313c35841d4dc88ab164d48b2108cd4b7e1eb27
Shoya <shoyabbb@gmail.com>
2023-11-07 23:13:59 +0900

    質問結果画面のUI構想

--- lib/view/conversation_content_creating_result_screen.dart
+++ lib/view/conversation_content_creating_result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:compa_app/widgets/back_ground_image_widget.dart';
+import 'package:compa_app/widgets/custom_elevated_button.dart';
 import 'package:compa_app/widgets/transparent_border_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -18,13 +19,29 @@ class _ConversationConteneCreatinngResultScreenState
       body: BackgroundImage(
         child: Center(
           child: TransparentBorder(
-            child: Text("テキストボックス"),
-            //質問数を出力
-            //回答者の名前
-            //回答者の名前はランダム
-            //質問内容を表示
-            //次の画面を表示ボタン
-            //質問設定ボタンを表示
+            child: Column(
+              children: [
+                Text("9問目‼︎"),
+                //質問数を出力
+                //問題数の分の値を表示
+                //回答者の名前
+                //前のページで入力した名前をランダムで表示させる
+                Text("XXさんが回答してね！！"),
+                //質問内容を表示
+                //選択した質問を前のページでランダムで表示するか、順番で表示するかを選択しているので、選択した内容で表示する
+                Text("あなたの性格を一言で表すと？？"),
+                //次の質問ボタンで、再ビルドする
+                CustomElevatedButton(
+                  text: "次の質問！",
+                  onPressed: () {},
+                ),
+                //質問設定ボタンを表示させ、押下したら前のページへ遷移する。
+                CustomElevatedButton(
+                  text: "質問内容の再設定",
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
